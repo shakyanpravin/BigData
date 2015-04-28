d69313234444c89e57e56c24c468b7129fd55463
shakyanpravin <shakyanpravin@hotmail.com>
2015-04-28 18:07:51 -0500

    website name changed in footer

--- BigData/src/bigdata/MapFold.java
+++ BigData/src/bigdata/MapFold.java
@@ -9,48 +9,54 @@ package bigdata;
  *
  * @author 984338
  */
-    public class MapFold {
- 
-	public static int f(int x){
-		return (isPrime(x)==1) ? 1 : 0;
-	}
-	
-	public static int g(int x, int y){
-		return x + y;
-	}
+public class MapFold {
 
-	
-	public static int isPrime(int n) {
-		if (n < 2) return 0;
-		for (int i = 2; i <= n / 2; i++) {
-			if (n % i == 0) return 0;
-		}
-		return 1;
-	}	
-	
-		
-	public static void main(String[] args) {
-		int[] a = {13, 11, 12, 16, 0, 21};
-		int[] b = new int[a.length];
-		int[] c = new int[a.length];
-		
-		for (int i = 0; i < a.length; i++){
-			b[i] = f(a[i]);
-		}
-		
-		int x = 0;
-		for (int i = 0; i < a.length; i++){
-			x = g(x, b[i]);
-			c[i] = x;
-		}
-		for (int i = 0; i < a.length; i++)
-			System.out.print("\t"+ a[i] + " ");
-		System.out.println();
-		for (int i = 0; i < a.length; i++)
-			System.out.print("\t"+ b[i] + " ");
-		System.out.println();
-		for (int i = 0; i < a.length; i++)
-			System.out.print("\t"+ c[i] + " ");
-		System.out.println();		
-	}
+    public static int f(int x) {
+        return (isPrime(x) == 1) ? 1 : 0;
+        //return (x % 2 == 0) ? 1 : -1;
+    }
+
+    public static int g(int x, int y) {
+        return x + y;
+    }
+
+    public static int isPrime(int n) {
+        if (n < 2) {
+            return 0;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {13, 11, 12, 16, 0, 21};
+        int[] b = new int[a.length];
+        int[] c = new int[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            b[i] = f(a[i]);
+        }
+
+        int x = 0;
+        for (int i = 0; i < a.length; i++) {
+            x = g(x, b[i]);
+            c[i] = x;
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("\t" + a[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("\t" + b[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("\t" + c[i] + " ");
+        }
+        System.out.println();
+    }
 }
